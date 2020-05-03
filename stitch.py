995e3064df1781bab53c76aedaa4004202b0f20d
wangyunhao <wangyunhao@cmlab.csie.ntu.edu.tw>
2020-05-04 01:53:41 +0800

    aligment

--- stitch.py
+++ stitch.py
@@ -12,9 +12,9 @@ class Stitch():
         self.images = images # bgr image list
         self.detection = detection
         self.gray_images = [] # gray image list
-        self.featrue_position = [] # 每張圖每個feature 的位置
+        self.feature_position = [] # 每張圖每個feature 的位置 harris only
         self.match = [] # i 與 i+1張圖之間的match match有(distance , index1, index2), index1是第i張圖第index1個feature
-        self.featrue = [] # 全部的image feature
+        self.feature = [] # 全部的image feature
         self.focal = focal # list of focal length
         self.rectangle = rectangle
         self.panorama = None
@@ -54,60 +54,71 @@ class Stitch():
                 #==================
                 '''
                 # feature descriptor
-                self.featrue_descriptor(img,np.where(R != 0))
+                self.feature_descriptor(img,np.where(R != 0))
         else:
-            sift = cv2.xfeatures2d.SIFT_create()
+            sift = cv2.xfeatures2d.SIFT_create(100)
             for i in range(len(self.images)):
                 img = cv2.cvtColor(self.images[i],cv2.COLOR_BGR2GRAY)
                 self.gray_images.append(img)
-            psd_kp1, psd_des1 = sift.detectAndCompute(self.gray_images[0],None)
-            psd_kp2, psd_des2 = sift.detectAndCompute(self.gray_images[1],None)
+                self.feature.append(sift.detectAndCompute(self.gray_images[i],None)) # (key, 128 vector)
+            
+            
+                    
+    def feature_descriptor(self, image, position):
+        if self.detection == 'harris':     
+            self.feature_position.append(position)
+            des = []
+            for i in range(len(position[0])):
+                f = image[position[0][i]-2:position[0][i]+3,position[1][i]-2:position[1][i]+3].flatten().astype(np.float64)
+                des.append(f)
+            self.feature.append(np.array(des)) # (25 vector)
+                
+    
+
+    def feature_matching(self):
+        if self.detection == 'harris':
+            for i in range(len(self.feature)-1):
+                tree = spatial.KDTree(self.feature[i])
+                match = []
+                for j in range(len(self.feature[i+1])):
+                    distance,index = tree.query(self.feature[i+1][j],2)
+                    if index[0] < 0.5*index[1]:
+                        match.append((distance[0],index[0],j)) # (distance, feature index, feature index)
+                match = sorted(match, key = lambda s: s[0])
+                self.match.append(match)
+                '''
+                # 拼接看看
+                for j in range(10):
+                    temp = np.concatenate((self.gray_images[i],self.gray_images[i+1]),axis=1)
+                    plt.plot(self.feature_position[i][1][match[j][1]],self.feature_position[i][0][match[j][1]],'-r*')
+                    plt.plot(384+self.feature_position[i+1][1][match[j][2]],self.feature_position[i+1][0][match[j][2]],'-r*')
+                plt.imshow(temp,cmap='gray')
+                plt.show()
+                '''
+        else:
             FLANN_INDEX_KDTREE = 1
             index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
             search_params = dict(checks=50)
 
             flann = cv2.FlannBasedMatcher(index_params, search_params)
-            matches = flann.knnMatch(psd_des1, psd_des2, k=2)
-            goodMatch = []
-            for m, n in matches:
-                if m.distance < 0.50*n.distance:
-                    goodMatch.append(m)
-                    #print(psd_kp1[m.queryIdx].pt,psd_kp2[m.trainIdx].pt)
+            for i in range(len(self.images)-1):
+                kp1,des1 = self.feature[i]
+                kp2,des2 = self.feature[i+1]
                     
-            # 增加一个维度
-            goodMatch = np.expand_dims(goodMatch, 1)
-            print(goodMatch.shape)
-
-            
-    def featrue_descriptor(self, image, position):
-        self.featrue_position.append(position)
-        des = []
-        for i in range(len(position[0])):
-            f = image[position[0][i]-2:position[0][i]+3,position[1][i]-2:position[1][i]+3].flatten().astype(np.float64)
-            des.append(f)
-        self.featrue.append(np.array(des))
-                
-    
-
-    def feature_matching(self):
-        for i in range(len(self.featrue)-1):
-            tree = spatial.KDTree(self.featrue[i])
-            match = []
-            for j in range(len(self.featrue[i+1])):
-                distance,index = tree.query(self.featrue[i+1][j],2)
-                if index[0] < 0.5*index[1]:
-                    match.append((distance[0],index[0],j))
-            match = sorted(match, key = lambda s: s[0])
-            self.match.append(match)
-
-            # 拼接看看
-            for j in range(10):
-                temp = np.concatenate((self.gray_images[i],self.gray_images[i+1]),axis=1)
-                plt.plot(self.featrue_position[i][1][match[j][1]],self.featrue_position[i][0][match[j][1]],'-r*')
-                plt.plot(384+self.featrue_position[i+1][1][match[j][2]],self.featrue_position[i+1][0][match[j][2]],'-r*')
-            plt.imshow(temp,cmap='gray')
-            plt.show()
-                     
+                matches = flann.knnMatch(des1, des2, k=2)
+                match = []
+                for m, n in matches:
+                    if m.distance < 0.50*n.distance:
+                        match.append((kp1[m.queryIdx].pt,kp2[m.trainIdx].pt))
+                self.match.append(match) #只有match沒有 feature position ((x1,y1) , (x2,y2))
+                '''
+                for j in range(10):
+                    temp = np.concatenate((self.gray_images[i],self.gray_images[i+1]),axis=1)
+                    plt.plot(match[j][0][0],match[j][0][1],'-r*')
+                    plt.plot(384+match[j][1][0],match[j][1][1],'-r*')
+                plt.imshow(temp,cmap='gray')
+                plt.show()
+                '''
         pass
 
     def cylindrical(self):
@@ -139,23 +150,45 @@ class Stitch():
         pass
 
     def pairwise_alignment(self):
-        A = np.zeros((20,6))
-        b = np.zeros((20,1))
-        for i in range(len(self.images)-1):
-            for j in range(10):
-                # origin x                                       # origin y
-                b[j*2,0] = self.featrue_position[i][1][self.match[i][j][1]]
-                b[j*2+1] = self.featrue_position[i][0][self.match[i][j][1]]
-                # match x                                             # match y
-                A[j*2,0:3] = np.array([self.featrue_position[i+1][1][self.match[i][j][2]], self.featrue_position[i+1][0][self.match[i][j][2]],1])
-                A[j*2+1,3:6] = np.array([self.featrue_position[i+1][1][self.match[i][j][2]], self.featrue_position[i+1][0][self.match[i][j][2]],1])
-            A_inverse = np.linalg.pinv(A)
-            motion_model = np.dot(A_inverse,b).reshape(2,3)
-            rows,cols = self.images[i+1].shape[:2]
-            result = cv2.warpAffine(self.images[i+1],motion_model,(rows,cols))
-            plt.imshow(result[:,:,::-1])
-            plt.show()
-            
+        panorama = np.zeros((self.images[0].shape[0],self.images[0].shape[1]*len(self.images)))
+        if self.detection == 'harris':
+            A = np.zeros((20,6))
+            b = np.zeros((20,1))
+            for i in range(len(self.images)-1):
+                for j in range(10):
+                    # origin x                                       # origin y
+                    b[j*2,0] = self.feature_position[i][1][self.match[i][j][1]]
+                    b[j*2+1] = self.feature_position[i][0][self.match[i][j][1]]
+                    # match x                                             # match y
+                    A[j*2,0:3] = np.array([self.feature_position[i+1][1][self.match[i][j][2]], self.feature_position[i+1][0][self.match[i][j][2]],1])
+                    A[j*2+1,3:6] = np.array([self.feature_position[i+1][1][self.match[i][j][2]], self.feature_position[i+1][0][self.match[i][j][2]],1])
+                A_inverse = np.linalg.pinv(A)
+                motion_model = np.dot(A_inverse,b).reshape(2,3)
+                rows,cols = self.images[i+1].shape[:2]
+                result = cv2.warpAffine(self.images[i+1],motion_model,(rows,cols))
+                plt.imshow(result[:,:,::-1])
+                plt.show()
+        else:
+            for i in range(len(self.images)-1):
+                A = np.zeros((20,6))
+                b = np.zeros((20,1))
+                for j in range(10):
+                    print(len(self.match),len(self.match[0]),len(self.match[0][0]))
+                    b[j*2,0] = self.match[i][j][0][0] # origin x
+                    b[j*2+1] = self.match[i][j][0][1] # origin y
+                    A[j*2,0:3] = np.array([self.match[i][j][1][0],self.match[i][j][1][1],1]) # x_prime
+                    A[j*2+1,3:6] = np.array([self.match[i][j][1][0],self.match[i][j][1][1],1]) # y_prime
+                print(A)
+                print(b)
+                A_inverse = np.linalg.pinv(A)
+                motion_model = np.dot(A_inverse,b).reshape(2,3)
+                print(motion_model)
+                rows,cols = self.images[i+1].shape[:2]
+                result = cv2.warpAffine(self.images[i+1],motion_model,(cols,rows))
+                plt.imshow(result[:,:,::-1])
+                plt.show()
+                break
+
         pass
 
     def fix_alignment(self, panorama):
@@ -182,5 +215,5 @@ if __name__ == '__main__':
     stitch_instance = Stitch(images ,args.detection ,focal, args.rectangle)
     #stitch_instance.cylindrical()
     stitch_instance.feature_detect()
-    #stitch_instance.feature_matching()
-    #stitch_instance.pairwise_alignment()
+    stitch_instance.feature_matching()
+    stitch_instance.pairwise_alignment()
